6a718e4b989292fa52c131b599daebea0da2c32c
unknown <surge0116@gmail.com>
2020-05-24 21:51:17 -0400

    done

---
+++ array-of-numbers.js
@@ -0,0 +1,12 @@
+const numbersAvg = [2,6,9,10,7,4,1,9]
+
+const averageNumbers = (numbers) => {
+    if (numbers.length === 0){
+        return null
+    }
+    // let sum = 0
+    // for (const number of numbers) {
+    //     sum =+ number
+    // }
+    return sumNumbers (numbers)/numbers.length
+}
